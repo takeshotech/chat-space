7263ece079c538fd857a85fc5afe583ef59a25f6
Takeshotech <dancedancedance0014ex@gmail.com>
2020-07-08 20:10:55 +0900

    incremental search

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
-# ルーティングを追加
 Rails.application.routes.draw do
   devise_for :users
-  # 下の行は削除する
-  # get 'messages/index
   root 'groups#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]do
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
 end
