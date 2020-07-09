314b86d2f18b7ebd555e6a54a0a757fc5a269aff
Takeshotech <dancedancedance0014ex@gmail.com>
2020-07-09 16:55:32 +0900

    incremental search2

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -17,6 +17,14 @@ $(function() {
     `;
     $("#user-search-result").append(html);
   }
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     $.ajax({
